f3eca0c5089f9485460e51ded621225d824cce85
PCIEG067 <PCIEG067@PCSNJ056.pcssom.net>
2014-10-22 16:11:04 -0400

    Method for calculating maximum and minimum value from data promise.
    This is useful for scaling, and other operations that require details
    surrounding the data for visualization purposes.

--- app/components/bar.js
+++ app/components/bar.js
@@ -11,12 +11,12 @@ angular.module('myApp.bar', ['ngRoute'])
 		   this._getDefaultBarData = function () {  
 			   return $timeout(function() {
 				   return [
-	                  {name: "Locke",    value:  4},
-	                  {name: "Reyes",    value:  8},
-	                  {name: "Ford",     value: 15},
-	                  {name: "Jarrah",   value: 16},
-	                  {name: "Shephard", value: 23},
-	                  {name: "Kwon",     value: 42}
+						{name: "Ford",     value: 15},
+						{name: "Jarrah",   value: 16},
+						{name: "Locke",    value:  4},
+						{name: "Reyes",    value:  8},
+						{name: "Shephard", value: 23},
+						{name: "Kwon",     value: 49}
 	                ];
 				   }, 2000);	//2000 ms = 2 seconds
 		   };
@@ -31,21 +31,39 @@ angular.module('myApp.bar', ['ngRoute'])
 			   {
 				   return 0;
 			   }
-		   }
+		   };
+		   
+		   this._calculateDataRanges = function (data, id) {
+		   //this function should calculate the mins and maxes of the dataset. Useful for things like scaling
+			   var sortedData = data.slice(0);	//clones array
+			   sortedData.sort(function(a, b){return a.value-b.value});
+			   console.log('calcing data ranges');
+			   console.log(sortedData);
+			   self[id].max = sortedData[sortedData.length - 1].value;
+			   self[id].min = sortedData[0].value;
+			   return {
+				   max: sortedData[sortedData.length - 1].value,
+				   min: sortedData[0].value, 
+			   };
+		   };
 		   
 		   //this function should get a new ID, and also initialize heatmapConfig for the current ID		
 		   //it also initializes the configuration details based upon the attributes fed into the function.
 		   this.initHeatmap = function (attrs) {
-		       self[currentID] = {};
+			   var id = currentID++;
+		       self[id] = {};
 		       if (0) { //if statement should parse attrs object, and determine where to grab data from
 		       }
 		       else { //if the data location is undefined, or not mentioned
-		    	   self[currentID].barData = self._getDefaultBarData();
+		    	   self[id].barData = self._getDefaultBarData();
+		    	   self[id].barData.then(
+		    			   function (data) {self._calculateDataRanges(data, id);},
+		    			   function (data) {alert('oops2');}
+    			   );
 		       }
+		       self[id].barScale = self._initBarScale(id, attrs);	//init
 		       
-		       self[currentID].barScale = self._initBarScale(currentID, attrs);	//init
-		       
-		       return currentID++;
+		       return id;
 		   };
 		   
 		   //discrete getters and setters
@@ -73,7 +91,7 @@ angular.module('myApp.bar', ['ngRoute'])
 	barService.getBarData($scope.barID).then( function(data) {
 		$scope.loading = '';
 		$scope.barData = data;
-		console.log(data);
+		
 		barWidth = barService.getBarWidth($scope.barID, {});
 		d3.select(".chart")
 		  .selectAll("div")
@@ -81,6 +99,8 @@ angular.module('myApp.bar', ['ngRoute'])
 		  .enter().append("div")
 		    .style("width", barWidth)
 		    .text(function(d) { return d.name; });
+		console.log(barService[$scope.barID]);
+		
 	}, function() { alert('oops');});
 	
 }]);
