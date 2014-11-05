2a06404eedbbeaab89f771f3c950b426ee1004ce
justin <auffant@pacificcontrols.net>
2014-11-05 14:16:16 -0500

    Completed a calendar object which may be instantiated into other html via <pci-calendar name="Insert Your New Scope.name here">

--- app/components/calendar.js
+++ app/components/calendar.js
@@ -1,42 +1,43 @@
 angular.module('myApp.calendar',['ui.bootstrap']);
 angular.module('myApp.calendar',['ngRoute'])
-  .controller('DatepickerDemoCtrl', function  ($scope) {
-	  $scope.today = function() {
-	    $scope.dt = new Date();
-	  };
-	  $scope.today();
-	
-	  $scope.clear = function () {
-	    $scope.dt = null;
-	  };
-	
-	  
-	
-
-	
-	  $scope.open = function($event) {
-	    $event.preventDefault();
-	    $event.stopPropagation();
-	
-	    $scope.opened = true;
-	    
-	    console.log($scope);
-	  };
-	
-	  $scope.dateOptions = {
-	    formatYear: 'yy',
-	    startingDay: 1
-	  };
-	
-	  $scope.format = 'yyyy-MM-dd';
-	})
+  
     .directive('myCalendar', function(){
 	  return{
 		  restrict: "E",
 		  scope:{
+			  name:"="
 		  },
 		  templateUrl : "views/calendar.html",
-			  
+		  controller: function  ($scope) {
+			  $scope.today = function() {
+				    $scope.dt = new Date();
+				  };
+				  $scope.today();
+				
+				  $scope.clear = function () {
+				    $scope.dt = null;
+				  };
+				
+				  $scope.log = function() {
+					  console.log($scope);
+				  };
+				  $scope.log();
+				  
+				  $scope.open = function($event) {
+						  
+				    $event.preventDefault();
+				    $event.stopPropagation();
+				
+				    $scope.opened = true;
+				  };
+				
+				  $scope.dateOptions = {
+				    formatYear: 'yyyy',
+				    startingDay: 0
+				  };
+				
+				  $scope.format = 'yyyy-MM-dd';
+				}
 	  }
   })
 ;
