d30fabaf35cac151ca0a5298a9393817bea0fe24
justin <auffant@pacificcontrols.net>
2014-11-05 14:21:22 -0500

    formatted code better

--- app/components/calendar.js
+++ app/components/calendar.js
@@ -1,43 +1,36 @@
 angular.module('myApp.calendar',['ui.bootstrap']);
 angular.module('myApp.calendar',['ngRoute'])
-  
-    .directive('myCalendar', function(){
-	  return{
-		  restrict: "E",
-		  scope:{
-			  name:"="
-		  },
-		  templateUrl : "views/calendar.html",
-		  controller: function  ($scope) {
-			  $scope.today = function() {
-				    $scope.dt = new Date();
-				  };
-				  $scope.today();
+  .directive('myCalendar', function(){
+    return{
+	  restrict: "E",
+	  scope:{
+	    name:"="
+	  },
+	  templateUrl : "views/calendar.html",
+	  controller: function  ($scope) {
+	    $scope.today = function() {
+		  $scope.dt = new Date();
+		};
+			
+		$scope.today();
+			
+		$scope.clear = function () {
+		  $scope.dt = null;
+		};
 				
-				  $scope.clear = function () {
-				    $scope.dt = null;
-				  };
+		$scope.open = function($event) {
+		  $event.preventDefault();
+		  $event.stopPropagation();
+		  $scope.opened = true;
+		};
 				
-				  $scope.log = function() {
-					  console.log($scope);
-				  };
-				  $scope.log();
-				  
-				  $scope.open = function($event) {
-						  
-				    $event.preventDefault();
-				    $event.stopPropagation();
+		$scope.dateOptions = {
+		  formatYear: 'yyyy',
+		  startingDay: 0
+		};
 				
-				    $scope.opened = true;
-				  };
-				
-				  $scope.dateOptions = {
-				    formatYear: 'yyyy',
-				    startingDay: 0
-				  };
-				
-				  $scope.format = 'yyyy-MM-dd';
-				}
+		$scope.format = 'yyyy-MM-dd';
 	  }
+    }
   })
 ;
