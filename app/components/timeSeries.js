9561f509f44603e8cd2c5ed47c6264f1a959cf30
justin <auffant@pacificcontrols.net>
2014-11-06 14:55:59 -0500

    updated time series to be able to redraw based on the calendar inputs

--- app/components/timeSeries.js
+++ app/components/timeSeries.js
@@ -3,115 +3,133 @@
 angular.module('myApp.timeSeries', ['ngRoute'])
 
 .controller('timeSeriesCtrl', ['$scope', '$location', '$route', 
-                    function($scope, $location, $route) {
+  function($scope, $location, $route) {
+	
 	$scope.timeSeries = 'timeSeries';
     d3.csv("expectedActual.csv", function(error, energyData) {
-    	$scope.log = function() {
-  		  console.log($scope);
-  	  	};
     	
-    	var composite = dc.compositeChart("#test_composed"); // names/creates the chart
-        
-    	var parse = d3.time.format("%m/%d/%Y").parse; // parses out the date object from the string
-    	var displayDate = d3.time.format("%m-%y"); // function to change the format of a date object to mm-yyyy
-    	
-        var ndx = crossfilter(energyData)
-        var totalSum = 0;
-        
-        var dateDimension = ndx.dimension(function(d) { return d3.time.month(parse(d.date));}) // creates the x-axis components using their date as a guide
-        var dailyDimension = ndx.dimension(function(d) { return d3.time.day(parse(d.date));})
-        
-        var actualGroup = dateDimension.group().reduceSum(function(d) { return d.actualKWH;}) // groups a value for each entry in the dimension by summing all the 'actualKWH' values of all objects within that dimension
-        var expectedGroup = dateDimension.group().reduceSum(function(e) { return +e.expectedKWH;}) // same as above with expectedKWH
-        var savingsGroup = dateDimension.group().reduceSum(function(e) { return +e.savings;}) // same as above with savings
-        var timeGroup = dailyDimension.group().reduceSum(function(e) { return +e.savings;})
-
-        var savingsSum = dateDimension.group().reduce( // groups a value for each entry in the dimension by finding the total aggregated savings
-          function(p,v) {totalSum = (+v.savings) + totalSum;  return totalSum;}, // sets the method for adding an entry into the total
-          function(p,v) {totalSum = totalSum-(+v.savings); return totalSum;}, // sets the method for removing an entry from the total
-          function() {totalSum = 0; return totalSum;}	 // sets the method for initializing the total
-        );
-        
-        var myDomain = d3.scale.linear().domain([minDate,maxDate]);
-        
-        $scope.domainX = function () { return domain;} // returnable domain, this feeds into the timeSeries.html page for the other charts
-        
-        var w = 900, // sets the width, height, margin, legend X and legend Y values
-    	    h = 680,
-    	    m = [75,150],
-    	    lX = (w-m[1])+25,
-    	    lY = (h)-650;
+    var myXUnits = d3.time.days;
     
-        var minDate = dateDimension.bottom(1)[0].date; // sets the lowest date value from the available data
-        var maxDate = dateDimension.top(1)[0].date; // sets the highest date value from the available data
-        
-        d3.select(".dc-chart"); // selects the chart
-        
-        var timeChart = dc.barChart("#time_chart")
-          .width(w)
-          .height(100)
-          .elasticX(false)
-          .elasticY(false)
-          .dimension(dailyDimension) // use the date Dimension for the objects
-          .colors('cyan')
-          .group(timeGroup, "Savings") // use the savings group for the grouped values
-          .x(myDomain)
-          .xUnits(d3.time.days)
-          .centerBar(true)
-        ;
-        
-        timeChart.margins().left = m[0];
-        timeChart.margins().right = m[1];
-        timeChart.xAxis().tickFormat(function(v) { return displayDate(new Date(v));});
-        timeChart.yAxis().ticks(0);
-        timeChart.xAxis().ticks(12);
-        
-        composite.margins().left = m[0]; // sets the left margin for the composite chart
-        composite.margins().right = m[1]; // sets the right margin for the composite chart
-        
-        composite.xAxis().tickFormat(function(v) {return displayDate(new Date(v));}); // sets the tick format to be the month/year only
-        composite.xAxis().ticks(12);
-        
-        composite // creates the graph object
-          .width(w) // sets width
-          .height(h) // sets height
-          .x(myDomain) // sets X axis
-          .elasticX(true) // allows X axis to be zoomed in/out
-          .elasticY(true)
-          .xUnits(d3.time.months)
-          .yAxisLabel("kW")
-          .legend(dc.legend().x(lX).y(lY).itemHeight(13).gap(5)) // legend position and add'l info
-          .renderHorizontalGridLines(true)
-          .mouseZoomable(true)
-          
-          .compose([
-            dc.barChart(composite) // creates the bar chart
-                .dimension(dateDimension) // use the date Dimension for the objects
-                .colors('cyan')
-                .group(savingsGroup, "Savings")// use the savings group for the grouped values
-                .centerBar(true)
-                .xAxisPadding(10)
-            ,
-            dc.lineChart(composite)
-                .dimension(dateDimension) // use the date dimension for the objects
-                .colors('blue')
-                .group(actualGroup, "Actual KWH")// use the savings group for the grouped values
-             , 
-             dc.lineChart(composite)
-                .dimension(dateDimension) // use the date dimension for the objects
-                .colors('red')
-                .group(expectedGroup, "Expected KWH")  // use the savings group for the grouped values
-            ,
-            dc.lineChart(composite)
-                .dimension(dateDimension) // use the date dimension for the objects
-                .colors('gray')
-                .group(savingsSum, "Total Savings/Waste") // use the savings group for the grouped values
-                .renderArea(true)
-            ])
-          .brushOn(false) // disables the fiddle/violin selection tool
-        ;
+    var getDomain = function(){
+      var myMinDate = $scope.startDate ? $scope.startDate : myDimension.bottom(1)[0].date;
+  	  var myMaxDate = $scope.endDate ? $scope.endDate : myDimension.top(1)[0].date; // sets the highest date value from the available data
+  	  		 
+  	  var tempDomain = d3.scale.linear().domain([myMinDate,myMaxDate]);
+  	  
+  	  return tempDomain;
+    };
+    
+    var myDomain, 
+        myDimension;
+    	
+    $scope.log = function() {
+  	  console.log($scope);
+  	};
+  	  		  	
+  	$scope.redraw = function(){
+  	  dc.renderAll();
+  	};
+  	  	
+  	$scope.setParams = function(){
+  		myDimension = dayDimension;
+  		myDomain = getDomain();
+  	  
+	  if(compositeChart !== undefined){
+	    myXUnits = d3.time.days;
+	    
+	    
+	    compositeChart();
+	  }
+	  else{
+		  myDimension = weekDimension;
+	  }
+  	};
+      
+  	console.log(dc);
+  	
+    var parse = d3.time.format("%m/%d/%Y").parse; // parses out the date object from the string
+    var displayDate = d3.time.format("%m-%d-%y"); // function to change the format of a date object to mm-yyyy
+    	
+    var ndx = crossfilter(energyData)
+    console.log(energyData);
 
-        dc.renderAll();
-        $scope.$apply() // no idea
+    var monthDimension = ndx.dimension(function(d) { return d3.time.month(parse(d.date));}) // creates the x-axis components using their date as a guide
+    var weekDimension = ndx.dimension(function(d) { return d3.time.week(parse(d.date));})
+    var dayDimension = ndx.dimension(function(d) { return d3.time.day(parse(d.date));})
+       
+    $scope.setParams();
+    
+    var actualGroup = myDimension.group().reduceSum(function(d) { return d.actualKWH;}) // groups a value for each entry in the dimension by summing all the 'actualKWH' values of all objects within that dimension
+    var expectedGroup = myDimension.group().reduceSum(function(e) { return +e.expectedKWH;}) // same as above with expectedKWH
+    var savingsGroup = myDimension.group().reduceSum(function(e) { return +e.savings;}) // same as above with savings
+    var timeGroup = dayDimension.group().reduceSum(function(e) { return +e.savings;})
+
+    var totalSum = 0;
+    var savingsSum = myDimension.group().reduce( // groups a value for each entry in the dimension by finding the total aggregated savings
+      function(p,v) {totalSum = (+v.savings) + totalSum;  return totalSum;}, // sets the method for adding an entry into the total
+      function(p,v) {totalSum = totalSum-(+v.savings); return totalSum;}, // sets the method for removing an entry from the total
+      function() {totalSum = 0; return totalSum;}	 // sets the method for initializing the total
+    );
+                
+    var w = 900, // sets the width, height, margin, legend X and legend Y values
+        h = 680,
+    	m = [75,150],
+    	lX = (w-m[1])+25,
+    	lY = (h)-650
+    ;
+        
+    
+       
+    var compositeChart = function(){
+      var composite = dc.compositeChart("#test_composed") // creates the graph object
+            .width(w) // sets width
+            .height(h) // sets height
+            .x(myDomain) // sets X axis
+            .elasticX(false) // allows X axis to be zoomed in/out
+            .elasticY(true)
+            .xUnits(myXUnits)
+            .yAxisLabel("kW")
+            .legend(dc.legend().x(lX).y(lY).itemHeight(13).gap(5)) // legend position and add'l info
+            .renderHorizontalGridLines(true)
+            .mouseZoomable(false)
+            .compose([
+              dc.barChart(composite) // creates the bar chart
+                  .dimension(myDimension) // use the date Dimension for the objects
+                  .colors('cyan')
+                  .group(savingsGroup, "Savings")// use the savings group for the grouped values
+                  .centerBar(true)
+                  .xAxisPadding(10)
+              ,
+              dc.lineChart(composite)
+                  .dimension(myDimension) // use the date dimension for the objects
+                  .colors('blue')
+                  .group(actualGroup, "Actual KWH")// use the savings group for the grouped values
+               , 
+               dc.lineChart(composite)
+                  .dimension(myDimension) // use the date dimension for the objects
+                  .colors('red')
+                  .group(expectedGroup, "Expected KWH")  // use the savings group for the grouped values
+              ,
+              dc.lineChart(composite)
+                  .dimension(myDimension) // use the date dimension for the objects
+                  .colors('gray')
+                  .group(savingsSum, "Total Savings/Waste") // use the savings group for the grouped values
+                  .renderArea(true)
+              ])
+            .brushOn(false) // disables the fiddle/violin selection tool
+          ;
+
+          composite.margins().left = m[0]; // sets the left margin for the composite chart
+          composite.margins().right = m[1]; // sets the right margin for the composite chart
+          
+          composite.xAxis().tickFormat(function(v) {return displayDate(new Date(v));}); // sets the tick format to be the month/year only
+                    
+          return composite;
+      };
+
+      compositeChart();
+  
+      $scope.redraw();
+      $scope.$apply() // no idea
     });
 }]);
