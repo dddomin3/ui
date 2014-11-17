51ea3c433d915b4a4c63e102f16a5e78e5facbdc
anthony <qian@pacificcontrols.net>
2014-11-17 15:14:13 -0500

    Added functionality to the facilityDetails page. Gives all information upon selection from a dropdown instead of specific ones. no multi facility view yet.

--- app/components/facilityDetails.js
+++ app/components/facilityDetails.js
@@ -17,12 +17,12 @@ angular.module('myApp.facilityDetails', ['ngRoute'])
 		//this function queries the server for all existing organizations
 		var message = {
 				"date": {
-			        "$gt": {
-			            "$date": "2014-10-22T22:02:48.488Z"
-			        }
-			    }
-			};
-		
+					"$gt": {
+						"$date": "2014-10-22T22:02:48.488Z"
+					}
+				}
+		};
+
 		return $http.post('http://10.239.3.132:9763/MongoServlet-0.0.1-SNAPSHOT/send', JSON.stringify(message))
 		.success(
 				function (data) {
@@ -33,61 +33,10 @@ angular.module('myApp.facilityDetails', ['ngRoute'])
 		)
 		.error( function () { alert('fail to query data'); } );
 	};
-	
 
-	
-	
-	
-	
-		
-		
-        for (var organization in _activeOrganizations) {	//separates each organization into its own group
-        	//if (_meterIsntConsumption(meterName)) { continue; }//if the dataset doesn't represent kWh data, do not make a group
-			var actualGroup = _dimensions.masterDimension.group().reduceSum(actualReduceSumGenerator(organization));
-			actualGroup.meterName = organization;	//saving metername on group
-        	_groups.actualGroups.push(actualGroup);
-        	
-        	var expectedGroup = _dimensions.masterDimension.group().reduceSum(expectedReduceSumGenerator(organization));
-			expectedGroup.meterName = organization;
-        	_groups.expectedGroups.push(expectedGroup);
-        	
-        	var savingsGroup =_dimensions.masterDimension.group().reduce(
-				//groups a value for each entry in the dimension by finding the total aggregated savings
-				savingsReduceAddGenerator(organization),	// sets the method for adding an entry into the total
-				savingsReduceRemoveGenerator(organization),	// sets the method for removing an entry from the total
-				savingsReduceInitialGenerator(organization)	// sets the method for initializing the total
-			);
-        	
-        	savingsGroup.meterName = organization;
-			_groups.savingsGroups.push(savingsGroup);
-			
-			var totalSum = 0;
-			var cumulativeSavingsGroup = _dimensions.masterDimension.group().reduce(
-					//groups a value for each entry in the dimension by finding the total aggregated savings
-	        		function(p,v) {
-	        			totalSum = (v.meterName === organization ? v.value*.15 : 0) + totalSum;
-	        			return totalSum;
-	        		},	// sets the method for adding an entry into the total
-	        		function(p,v) {
-	        			totalSum = totalSum - (v.meterName === organization ? v.value*.15 : 0 );
-	        			return totalSum;
-	        		},	// sets the method for removing an entry from the total
-	        		function() {
-	        			totalSum = 0;
-	        			return totalSum;
-	        		}	// sets the method for initializing the total
-				);
-			cumulativeSavingsGroup.meterName = organization;
-			_groups.cumulativeSavingsGroups.push(cumulativeSavingsGroup);
-        }
-	
-	
-	
-	
-	
 
-	
-	
+
+
 	var _getOrganizationQuery =  function () {
 		return _organizationQuery;
 	};
@@ -99,34 +48,186 @@ angular.module('myApp.facilityDetails', ['ngRoute'])
 	};
 	var _initActiveOrganization = function (organization) {
 		_activeOrganizations[organization] = {
+				"name" : organization,
 				"actual" : "",
 				"expected" : "",
 				"meterQuery" : {}
 		};
-		_getMeters(organization);
+
 	};
-	
+
 	_servObj = {
-		
-		getOrganizations: _getOrganizations,
-		
-		
-		
-		
-		getOrganizationQuery : _getOrganizationQuery,
-		
+
+			getOrganizations: _getOrganizations,
+
+
+			getActiveOrganizations : _getActiveOrganizations,
+			initActiveOrganization : _initActiveOrganization,
+			getOrganizationQuery : _getOrganizationQuery,
+			deleteActiveOrganization : _deleteActiveOrganization 
 	};
-	
+
 	return _servObj;
 }])
-.controller('facilityDetailsCtrl', ['$scope', '$location', '$route', 'facilityDetailsService', 
-                    function($scope, $location, $route, dataService) {
-	
-	
+
+
+
+
+.factory('csvReaderService', ['$http', function($http){
+
+//	parser copypasta'ed from internet
+	function CSVToArray( strData, strDelimiter ){
+		strDelimiter = (strDelimiter || ",");
+
+		var objPattern = new RegExp(
+				(
+						"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+						"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+						"([^\"\\" + strDelimiter + "\\r\\n]*))"
+				),
+				"gi"
+		);
+		var arrData = [[]];
+		var arrMatches = null;
+		while (arrMatches = objPattern.exec( strData )){
+			var strMatchedDelimiter = arrMatches[ 1 ];
+			if (
+					strMatchedDelimiter.length &&
+					strMatchedDelimiter !== strDelimiter
+			){
+				arrData.push( [] );
+			}
+			var strMatchedValue;
+			if (arrMatches[ 2 ]){
+				strMatchedValue = arrMatches[ 2 ].replace(
+						new RegExp( "\"\"", "g" ),
+						"\""
+				);
+
+			} else {
+				strMatchedValue = arrMatches[ 3 ];
+			}
+			arrData[ arrData.length - 1 ].push( strMatchedValue );
+		}
+		return( arrData );
+	}
+	//end parser copypasta'ed from internet
+	var Url  = "http://10.239.3.132:9763/MongoServlet-0.0.1-SNAPSHOT/send";
+	var _csvArray = [];
+
+	// $httpProvider.defaults.headers.get = { 'Collections' : 'Facility' }
+
+	var requestString = "{}";
+	var config = {
+			method:'POST',
+			headers: {'Collection': 'Facility'},
+			url:Url,
+			data:requestString
+	}
+
+
+
+
+	var _promise = $http(config).success(function(response,status,headers,config){
+
+		//_csvArray = CSVToArray(response,",");
+		_csvArray = response;
+	});
+	//console.log(promise);
+
+
+
+	var _servObj = {
+			promise :  _promise,
+			csvArray: function(){return _csvArray}
+	};
+
+	return _servObj;
+}])
+
+.controller('facilityDetailsCtrl', ['$scope', '$location', '$route', 'facilityDetailsService', 'csvReaderService',
+                                    function($scope, $location, $route, dataService,csvArray) {
+
+	$scope.activeOrganizations = dataService.getActiveOrganizations();
 	$scope.queryOrganizations = function () {
 		dataService.getOrganizations().then( function () {
 			$scope.organizationQuery = dataService.getOrganizationQuery();
 		});
 	};
+
+	csvArray.promise.then( function (d) {
+		$scope.thisCsvArray = csvArray.csvArray();
+
+		console.log($scope.thisCsvArray.result);
+
+
+
+	});
+
+	$scope.initMultiOrganization = function (organization) {
+		var add = true;
+		for(var thisOrg in $scope.activeOrganizations){
+
+			if(thisOrg===organization){
+				dataService.deleteActiveOrganization(organization);
+				add=false;
+			}
+		}
+		if(add===true){
+			dataService.initActiveOrganization(organization);}
+	};
+
+	$scope.initSingleOrganization = function (organization) {
+
+		for(var thisOrg in $scope.activeOrganizations){
+
+
+			dataService.deleteActiveOrganization(thisOrg);
+
+
+		}
+var scopeArray = [];
+var urlScopeArray = [];
+		dataService.initActiveOrganization(organization);
+		for(var a in $scope.thisCsvArray.result){
+			//console.log($scope.thisCsvArray.result[a].clientName);
+			for(var c in $scope.activeOrganizations){var activeOrgString = $scope.activeOrganizations[c].name;}
+			console.log(organization);
+console.log($scope.thisCsvArray.result[a].stationName);
+			//console.log($scope.thisCsvArray.result[a].clientName === activeOrgString);
+			if($scope.thisCsvArray.result[a].stationName === organization){
+				for(var b in $scope.thisCsvArray.result[a]){
+				
+					//console.log($scope.thisCsvArray.result[a][b]);
+					scopeArray.push($scope.thisCsvArray.result[a][b]);
+					//urlScopeArray.push('<a href="' + $scope.thisCsvArray.result[a][b] + '">' + $scope.thisCsvArray.result[a][b] + '</a>');
+					$scope.displayThis = scopeArray;
+					//$scope.urlDisplayThis = urlScopeArray;
+					
+				}
+				return;
+			}
+			else{
+			$scope.displayThis = "";
+			}
+		}
+	};
+
+	$scope.isLink = function(string){
+	//var regex = /^[a-z](?:[-a-z0-9\+\.])*:(?:\/\/(?:(?:%[0-9a-f][0-9a-f]|[-a-z0-9\._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!\$&'\(\)\*\+,;=:])*@)?(?:\[(?:(?:(?:[0-9a-f]{1,4}:){6}(?:[0-9a-f]{1,4}:[0-9a-f]{1,4}|(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3})|::(?:[0-9a-f]{1,4}:){5}(?:[0-9a-f]{1,4}:[0-9a-f]{1,4}|(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3})|(?:[0-9a-f]{1,4})?::(?:[0-9a-f]{1,4}:){4}(?:[0-9a-f]{1,4}:[0-9a-f]{1,4}|(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3})|(?:[0-9a-f]{1,4}:[0-9a-f]{1,4})?::(?:[0-9a-f]{1,4}:){3}(?:[0-9a-f]{1,4}:[0-9a-f]{1,4}|(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3})|(?:(?:[0-9a-f]{1,4}:){0,2}[0-9a-f]{1,4})?::(?:[0-9a-f]{1,4}:){2}(?:[0-9a-f]{1,4}:[0-9a-f]{1,4}|(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3})|(?:(?:[0-9a-f]{1,4}:){0,3}[0-9a-f]{1,4})?::[0-9a-f]{1,4}:(?:[0-9a-f]{1,4}:[0-9a-f]{1,4}|(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3})|(?:(?:[0-9a-f]{1,4}:){0,4}[0-9a-f]{1,4})?::(?:[0-9a-f]{1,4}:[0-9a-f]{1,4}|(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3})|(?:(?:[0-9a-f]{1,4}:){0,5}[0-9a-f]{1,4})?::[0-9a-f]{1,4}|(?:(?:[0-9a-f]{1,4}:){0,6}[0-9a-f]{1,4})?::)|v[0-9a-f]+[-a-z0-9\._~!\$&'\(\)\*\+,;=:]+)\]|(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:\.(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}|(?:%[0-9a-f][0-9a-f]|[-a-z0-9\._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!\$&'\(\)\*\+,;=@])*)(?::[0-9]*)?(?:\/(?:(?:%[0-9a-f][0-9a-f]|[-a-z0-9\._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!\$&'\(\)\*\+,;=:@]))*)*|\/(?:(?:(?:(?:%[0-9a-f][0-9a-f]|[-a-z0-9\._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!\$&'\(\)\*\+,;=:@]))+)(?:\/(?:(?:%[0-9a-f][0-9a-f]|[-a-z0-9\._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!\$&'\(\)\*\+,;=:@]))*)*)?|(?:(?:(?:%[0-9a-f][0-9a-f]|[-a-z0-9\._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!\$&'\(\)\*\+,;=:@]))+)(?:\/(?:(?:%[0-9a-f][0-9a-f]|[-a-z0-9\._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!\$&'\(\)\*\+,;=:@]))*)*|(?!(?:%[0-9a-f][0-9a-f]|[-a-z0-9\._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!\$&'\(\)\*\+,;=:@])))(?:\?(?:(?:%[0-9a-f][0-9a-f]|[-a-z0-9\._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!\$&'\(\)\*\+,;=:@])|[\x{E000}-\x{F8FF}\x{F0000}-\x{FFFFD}|\x{100000}-\x{10FFFD}\/\?])*)?(?:\#(?:(?:%[0-9a-f][0-9a-f]|[-a-z0-9\._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!\$&'\(\)\*\+,;=:@])|[\/\?])*)?$/i
+var regex = /^(ht|f)tps?:\/\/[a-z0-9-\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?$/
+	return regex.test(string);
+	};
+	
+	$scope.urlify = function(string){
+	//var regex = /^(ht|f)tps?:\/\/[a-z0-9-\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?$/
+	return "'+string+'";
+	}
+	
+	$scope.debug = function () {
+		console.log($scope.thisCsvArray);
+
+	};
 	$scope.queryOrganizations();
+	$scope.apply;
 }]);
