d331aa4f297ee5295978196866e8ebb5c37ea64a
brian <buccellato@pacificcontrols.net>
2014-12-05 14:26:58 -0500

    Work Order Grid: Added functionality to show columns which had been hidden without reloading the page. Also provided an example of how to redraw a ui-grid table dynamically (see the comment at the bottom of workOrderGrid.js)

--- app/components/workOrderGrid.js
+++ app/components/workOrderGrid.js
@@ -78,26 +78,71 @@ angular.module('myApp.workOrderGrid', ['ngRoute', 'ui.grid', 'ui.grid.edit', 'ui
 		});
 		$scope.gridOptions = {
 			enableSorting: true,
+			onRegisterApi: function(gridApi){
+				$scope.gridApi = gridApi;
+			},
 			enableFiltering: true,
 			multiSelect: false,
 			data: 'eventData',
 			rowTemplate: '<div ng-click="getExternalScopes().showMessage(row.entity)"  ng-repeat="col in colContainer.renderedColumns track by col.colDef.name" class="ui-grid-cell" ui-grid-cell></div>',
 			columnDefs: [
-			             {field: 'organization', displayName: 'Organization'},
-			             {field: 'facility', displayName: 'Facility'},
-			             {field: 'asset', displayName: 'Asset'},
-			             {field: 'anomaly', displayName: 'Anomaly'},
-			             {field: 'createdTime', displayName: 'Ticket Creation Date'},
-			             {field: 'closedTime', displayName: 'Ticket Closing Date'},
-			             {field: 'eventID', displayName: 'Event ID'},
+			             {field: 'organization', displayName: 'Organization', visible: true},
+			             {field: 'facility', displayName: 'Facility', visible: true},
+			             {field: 'asset', displayName: 'Asset', visible: true},
+			             {field: 'anomaly', displayName: 'Anomaly', visible: true},
+			             {field: 'createdTime', displayName: 'Ticket Creation Date', visible: true},
+			             {field: 'closedTime', displayName: 'Ticket Closing Date', visible: true},
+			             {field: 'eventID', displayName: 'Event ID', visible: true},
 			             ]
 	
 		};
 		
-		$scope.reset = function(){
-			$location.templateUrl = "/views/workOrderGrid";
+		$scope.cleanPoo = function(){
+			alert("Are you sure you want to change this chart???");
+			$location.url('\workOrderGrid');
 			$route.reload();
 		}
+		
+		$scope.reset = function(){
+			for(var i=0;i<$scope.gridOptions.columnDefs.length;i++){
+				$scope.gridOptions.columnDefs[i].visible = true;
+			}
+			$scope.gridApi.grid.refresh();
+		}
+		
+		 
+		 /*Below is how you redraw a ui-grid chart dynamically.  Apparently, the 'data' field is just a 
+		  * reference to the data used in the chart.  This reference persists, so attempting
+		  * something like $scope.gridOptions.data = something different; then refreshing will 
+		  * have no effect but changing the variable on the scope ('eventData' in this case)
+		  * and then refreshing (really wonky methodology BTW) will redraw the chart with new
+		  * data, column definitions etc.  Also, the 'onRegisterApi: yada, yada' is not needed the second time.
+		  * Since there really is just one chart, the initial placement on the scope is sufficient */ 
+		  /*
+		   * 
+		   *$scope.reset = function(){
+				for(var i=0;i<$scope.gridOptions.columnDefs.length;i++){
+					$scope.gridOptions.columnDefs[i].visible = true;
+					
+				}
+				$scope.eventData = [{name: "Butt", age: "Head"}, {name: "Bea", age: "Vis"}];
+				$scope.gridOptions = {
+						enableSorting: true,
+						/*onRegisterApi: function(gridApi){
+							$scope.gridApi = gridApi;
+						},
+						enableFiltering: true,
+						multiSelect: false,
+						data: 'eventData',
+						rowTemplate: '<div ng-click="getExternalScopes().showMessage(row.entity)"  ng-repeat="col in colContainer.renderedColumns track by col.colDef.name" class="ui-grid-cell" ui-grid-cell></div>',
+						columnDefs: [
+						             {field: 'name', displayName: 'First Name', visible: true},
+						             {field: 'age', displayName: 'Last Name', visible: true}
+						             ]
+				
+					};
+				$scope.gridApi.grid.refresh();
+			}*/	
 }])
 
 .controller('workOrderGridConfigCtrl', [ function(){
