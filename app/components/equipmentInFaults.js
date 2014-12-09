df6ad85c17f33575706e6206a09450fcb6d6aa98
ray <wang@pacificcontrols.net>
2014-12-09 18:20:21 -0500

    add function to show # of anomalies,first occ, last occ,# of occ,total waste, total potential saving

--- app/components/equipmentInFaults.js
+++ app/components/equipmentInFaults.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('myApp.equipmentInFaults', ['ngRoute'])
 
-.factory('equipmentInFaultsService', ['$http', function($http){
+.factory('equipmentInFaultsService', ['$http', '$q',function($http,$q){
 	var print_filter = function (filter){
 		var f=eval(filter);
 		if (typeof(f.length) != "undefined") {}else{}
@@ -18,6 +18,9 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 	var _assetQuery = {};
 	
 	var _location={};
+	
+
+
 	
 
 	
@@ -164,6 +167,7 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 				
 		)
 		.error( function () { alert('fail to query data'); } );
+		
 	};
 	
 	
@@ -277,6 +281,61 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 	//query Event Data
 	
 	
+	var _queryEvents = function (org,asset) {	
+		//this function queries the server for all existing Assets
+		var _event=[];
+		var caller =this;
+		var Url  = "http://10.239.3.132:9763/MongoServlet-0.0.1-SNAPSHOT/send";
+		var requestString = "{\"asset\":\"" +asset +"\","+"\"stationName\":\""+org+"\"}";
+		var config = {
+				method:'POST',
+				headers: {'Collection': 'Event'},
+				url:Url,
+				data:requestString
+		}
+		
+		return $http(config)
+		.success(
+				function (data) {
+
+					if(data.result!=null){
+						for(var i = 0, ilen = data.result.length; i < ilen; i++) { 
+							
+							var _subEvent=[];
+				
+							_subEvent.push(data.result[i].source);
+							_subEvent.push(data.result[i].asset);
+							_subEvent.push(data.result[i].createdTime);
+							_subEvent.push(data.result[i].waste);
+							_subEvent.push(data.result[i].potentialSaving)
+							
+							
+							_event.push(_subEvent);
+							
+							
+							caller.data._event=_event;	
+							
+							
+													
+	
+							
+							};	
+					};
+					
+					
+					
+				})
+		.error( function () { alert('fail to query data');
+		
+		} );
+	
+		
+	};
+	
+	// get events
+	var _getEvents = function(){
+		return _event;
+	};
 	
 	
 	
@@ -318,7 +377,11 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 			
 			getLocation : _getLocation,
 			
-			getAddress : _getAddress
+			getAddress : _getAddress,
+			
+			getEvents: _getEvents,
+			
+			queryEvents: _queryEvents
 			
 	};
 
@@ -349,10 +412,8 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 
 	var _promise = $http(config).success(function(response,status,headers,config){
 
-		//_csvArray = CSVToArray(response,",");
 		_queryData = response;
 	});
-	//console.log(promise);
 
 
 
@@ -364,17 +425,25 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 	return _servObj;
 }])
 
-.controller('equipmentInFaultsCtrl', ['$scope', '$modal', '$location', '$route', 'equipmentInFaultsService', 'facilityQueryService','sharedPropertiesService',
-                                    function($scope, $modal, $location, $route, dataService,queryData,sharedProperties) {
+.controller('equipmentInFaultsCtrl', ['$scope', '$modal', '$location', '$route', 'equipmentInFaultsService', 'facilityQueryService','sharedPropertiesService','$q','$http',
+                                    function($scope, $modal, $location, $route, dataService,queryData,sharedProperties,$q,$http) {
 
 	$scope.activeOrganization = "";
 	$scope.activeClient="";
 	$scope.activeAsset="";
 	$scope.active={};
 	$scope.activeInfo={};
+	$scope.events=[];
 	
+	
+	var _events=[];
+	
+	var vm=this;
+	
+	vm.data={};
+	
+	angular.extend(vm,dataService);
 
-	
 	
 	
 	$scope.queryOrganizations = function () {
@@ -396,6 +465,16 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 			$scope.assetQuery = dataService.getAssetQuery();
 		});
 	};
+	
+	//get events data
+
+/*	$scope.queryEvents = function (activeOrganization,activeAsset){
+		promised = dataService.queryEvents(activeOrganization,activeAsset).then(function(){
+			$scope.eventsTemp=dataService.getEvents();
+			
+		});
+	
+	};*/
 	
 
 	
@@ -531,7 +610,7 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 	
 	
 	
-	// put the select asset info into active map
+	// put the select asset info into active map, query data happens in this step and store the queried data for all assets in specific place
 	$scope.apply=function(){
 		$scope.active=sharedProperties.getActives();
 		
@@ -545,6 +624,8 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 			info.push(tempArray[1]);
 			info.push(tempArray[2]);
 			
+			var deferred= $q.defer();
+			
 			dataService.getLocation(tempArray[1],tempArray[0]).then(function(){
 				info.push( dataService.getAddress() );
 				
@@ -552,13 +633,18 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 			
 			info.push(tempArray[3]);
 			// be careful , the sequence of the array doesnot follow the the sequence of pushing action, in this case, info[4] is address and info[3] is tempArray[3]			
-			console.log(tempArray[3]);
+			
+			
+			
+			
+			
+			info.push($scope.reduce(tempArray[1],tempArray[2]));
+			
 			$scope.activeInfo[key]=info;
 			
+			console.log($scope);
 			
 		});
-		
-		
 		
 		
 
@@ -567,7 +653,7 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 	
 	$scope.j=0;
 	
-	//function to select / unselect all assets under one asset type
+	//function to select/unselect all assets under one asset type
 	$scope.selectChildren=function(placeholder){
 		var y=document.getElementsByName("activeList");
 		
@@ -594,11 +680,99 @@ angular.module('myApp.equipmentInFaults', ['ngRoute'])
 			
 		}
 		$scope.j=$scope.j+1;
-		console.log($scope);
 		
 	}
+	
+	
 
 	
+	// Reduce function that takes evnets data from database and output total data for each AHU and put it into activeinfo map
+	
+
+	$scope.reduce = function(activeOrganization,activeAsset){
+		var anomaly=0;
+		var anomalyMap={};
+		var firstOcc="";
+		var lastOcc="";
+		var createdTime=[];
+		var occ=0;
+		var waste=0;
+		var potentialSaving=0;
+		var eventsInfo=[];
+		
+
+		
+
+		console.log(vm);
+		
+		vm.queryEvents(activeOrganization,activeAsset).then().finally(function(){
+			console.log(vm.data._event);
+			
+			
+			angular.forEach(vm.data._event,function(value,key){
+				//write the source into the map
+				anomalyMap[value[0]]="";
+				
+				// write the created time into an array
+				
+				createdTime.push(new Date ( value[2] ) );
+				
+	
+				
+				
+				
+			
+			// calculate the total number of ticket occurrences
+			occ++;
+			
+			//calculate the total waste and saving
+			
+			waste= waste + parseFloat(value[3]);
+			potentialSaving=potentialSaving + parseFloat(value[4]);
+		});
+		
+		// get the lenght of the anomalyMap to get the total number of anomaly types 
+		angular.forEach(anomalyMap,function(value,key){
+			anomaly++;
+		});
+		
+		
+		//sort the creaed time array and get the first and last occ ticket time
+		
+		createdTime=createdTime.sort();
+		firstOcc=createdTime[0];
+		lastOcc=createdTime[createdTime.length-1];
+		
+		console.log(anomaly);
+		console.log(createdTime);
+		console.log(occ);
+		console.log(waste);
+		console.log(potentialSaving);
+		
+		eventsInfo.push(anomaly);
+		eventsInfo.push(firstOcc);
+		eventsInfo.push(lastOcc);
+		eventsInfo.push(occ);
+		eventsInfo.push(waste);
+		eventsInfo.push(potentialSaving);
+		
+		
+
+		return eventsInfo;
+			
+		});
+		
+	
+
+
+		 
+		
+		
+	
+	}
+	
+	
+
 	
 	
 }])
